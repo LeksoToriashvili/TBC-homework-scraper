408be0a19fd23ca808cfae5d003effc65cd446cd
LeksoToriashvili <ltoriashvili@gmail.com>
2024-09-28 16:26:21 +0400

    change scraper using threads

--- scraper.py
+++ scraper.py
@@ -1,28 +1,72 @@
-from bs4 import BeautifulSoup
 import requests
+from concurrent.futures import ThreadPoolExecutor
+from bs4 import BeautifulSoup
 from pymongo import MongoClient
-
-URL = "https://kulinaria.ge/receptebi/cat/konservi-da-sousebi/"
-BASE_URL = "https://kulinaria.ge"
+import os
 
 
-def write_to_database(database, documents):
-    client = MongoClient(database)
-    db = client["scrape"]
+class Scraper:
+    def __init__(self, uri, database, collection):
+        self.uri = uri
+        self.database = database
+        self.collection = collection
+        self._URL = "https://kulinaria.ge/receptebi/cat/konservi-da-sousebi/?page="
+        self._BASE_URL = "https://kulinaria.ge"
+        self.max_number_of_requests = 100
+        self._recipes = []
+        self._status = ""
 
-    if "scraped_data" in db.list_collection_names():
-        db["scraped_data"].drop()
+    def scrape(self):
+        try:
+            links = self.__fetch_links()
+            self.__fetch_recipes(links)
+            self.__store_data()
+            self._status = "success"
+        except Exception as e:
+            self._status = e
 
-    collection = db["scraped_data"]
+    def __fetch_links(self):
+        i = 0
+        links = set()
 
-    collection.insert_many(documents)
-    client.close()
+        while True:
+            i += 1
+            if i > self.max_number_of_requests:
+                break
 
+            response = requests.get(self._URL + str(i))
 
-def fetch_recipies(links):
-    result = []
+            soup = BeautifulSoup(response.text, 'html.parser')
+            divs = soup.find_all('div', class_='box__img')
+            new_links = set()
+            for div in divs:
+                new_links.add(self._BASE_URL + div.find('a', href=True)['href'])
 
-    for link in links:
+            if links == links | new_links:
+                break
+
+            links = links.union(new_links)
+
+        return list(links)
+
+    def __fetch_recipes(self, links):
+        with ThreadPoolExecutor(max_workers=os.cpu_count()) as executor:
+            for link in links:
+                executor.submit(self.__fetch_recipe, link)
+
+    def __store_data(self):
+        client = MongoClient(self.uri)
+        db = client[self.database]
+
+        if self.collection in db.list_collection_names():
+            db[self.collection].drop()
+
+        collection = db[self.collection]
+
+        collection.insert_many(self._recipes)
+        client.close()
+
+    def __fetch_recipe(self, link):
         document = {}
         response = requests.get(link)
         soup = BeautifulSoup(response.text, "html.parser")
@@ -30,10 +74,11 @@ def fetch_recipies(links):
         document['title'] = soup.find("div", class_="post__title").find("h1").text
         document['url'] = link
         document['category_title'] = soup.find("div", class_="pagination-container").find_all("a")[2].text
-        document['category_url'] = BASE_URL + soup.find("div", class_="pagination-container").find_all("a")[2]["href"]
+        document['category_url'] = self._BASE_URL + soup.find("div", class_="pagination-container").find_all("a")[2]["href"]
         document['subcategory_title'] = soup.find("div", class_="pagination-container").find_all("a")[3].text
-        document['subcategory_url'] = BASE_URL + soup.find("div", class_="pagination-container").find_all("a")[3]["href"]
-        document['img_url'] = BASE_URL + soup.find("div", class_="post__img").find("img")["src"]
+        document['subcategory_url'] = self._BASE_URL + soup.find("div", class_="pagination-container").find_all("a")[3][
+            "href"]
+        document['img_url'] = self._BASE_URL + soup.find("div", class_="post__img").find("img")["src"]
         document['description'] = soup.find("div", class_="post__description").text.strip()
         document['author'] = soup.find("div", class_="post__author").find("a").text.strip()
 
@@ -54,24 +99,8 @@ def fetch_recipies(links):
         document['ingredients'] = ingredients
         document['steps'] = steps
 
-        result.append(document)
+        self._recipes.append(document)
 
-    return result
-
-
-def fetch_links(url):
-    response = requests.get(url)
-
-    soup = BeautifulSoup(response.text, 'html.parser')
-    divs = soup.find_all('div', class_='box__img')
-    links = []
-    for div in divs:
-        links.append(BASE_URL + div.find('a', href=True)['href'])
-
-    return links
-
-
-def scrape(database):
-    links = fetch_links(URL)
-    documents = fetch_recipies(links)
-    write_to_database(database, documents)
+    @property
+    def status(self):
+        return self._status
